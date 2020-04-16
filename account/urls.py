2e79aa34eb5c1d5fb5a9e6bb85b36bb4c6b63e8c
David <davidgao@live.ca>
2020-04-16 16:03:55 -0400

    finished chat, added Account, profile pictures
    Account is a onetoone field with Users that holds additional information (profile picture)... Chat displays all user chat rooms. Also registered all Models

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.profile, name='account'),
     path('login', views.login_action, name='login'),
     path('signup', views.signup_action, name='signup'),
     path('logout', views.logout_action, name='logout'),
--- listing/admin.py
+++ listing/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Listing
+from .models import PurchasedListing
+
 
 # Register your models here.
 admin.site.register(Listing)
+admin.site.register(PurchasedListing)
+
