78b090f227d2524270d74a3a275339e9eaeba237
David <davidgao@live.ca>
2020-04-21 16:19:06 -0400

    form validation fixes, other minor visual changes

--- transaction/models.py
+++ transaction/models.py
@@ -11,7 +11,7 @@ class Transaction(models.Model):
 	date = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
-		return self
+		return str(self.date)
 
 	def save(self, *args, **kwargs):
 		super(Transaction, self).save(*args, **kwargs)
